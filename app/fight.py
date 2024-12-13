e85658dcc32390e5defeaa852e5e5457584e3bd0
halyna <1208petrova@gmail.com>
2024-12-13 12:29:23 +0200

    solution my

--- app/fight.py
+++ app/fight.py
@@ -1,8 +1,7 @@
-from app import knights
 from app.knight.one_knight import Knight
 
 
-def battle(knight_1: Knight, knight_2: Knight) -> dict:
+def fight(knight_1: Knight, knight_2: Knight) -> dict:
     knight_1.hp -= knight_2.power - knight_1.protection
     knight_2.hp -= knight_1.power - knight_2.protection
     battle_result_hp = {
@@ -10,58 +9,3 @@ def battle(knight_1: Knight, knight_2: Knight) -> dict:
         knight_2.name: knight_2.hp
     }
     return battle_result_hp
-
-
-#
-# first = {
-#     "arthur": {
-#         "name": "Arthur",
-#         "power": 45,
-#         "hp": 75,
-#         "armour": [
-#             {
-#                 "part": "helmet",
-#                 "protection": 15,
-#             },
-#             {
-#                 "part": "breastplate",
-#                 "protection": 20,
-#             },
-#             {
-#                 "part": "boots",
-#                 "protection": 10,
-#             }
-#         ],
-#         "weapon": {
-#             "name": "Two-handed Sword",
-#             "power": 55,
-#         },
-#         "potion": None,
-#     },
-#     }
-#
-# second = {"Red Knight":
-#               {"name": "Red Knight",
-#         "power": 40,
-#         "hp": 70,
-#         "armour": [
-#             {
-#                 "part": "breastplate",
-#                 "protection": 25,
-#             }
-#         ],
-#         "weapon": {
-#             "name": "Sword",
-#             "power": 45
-#         },
-#         "potion": {
-#             "name": "Blessing",
-#             "effect": {
-#                 "hp": +10,
-#                 "power": +5,
-#             },
-#         },},}
-#
-# Knight.one_knight_create(first)
-# Knight.one_knight_create(second)
-# battle(Knight.one_knight_create(first), Knight.one_knight_create(second))
--- app/knight/all_knights.py
+++ app/knight/all_knights.py
@@ -1,15 +1,9 @@
-from app import knights
-from app.knight.one_knight_crate import one_knight_create
+from app.knight.one_knight import Knight
 
 
-def all_knights_create(all_knights_dict: dict):
+def all_knights_create(all_knights_dict: dict) -> list:
     knights_list = []
     for i in all_knights_dict.values():
-        one_knight = one_knight_create({i["name"]: i})
+        one_knight = Knight.one_knight_create({i["name"]: i})
         knights_list.append(one_knight)
     return knights_list
-
-
-knights_dict = knights.KNIGHTS
-
-print(all_knights_create(knights_dict)[1].power)
